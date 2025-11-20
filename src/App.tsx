7610d985aeead4346186a1501d3dc73cef87f19d
MerchantHausLLC <admin@merchanthaus.io>
2025-11-19 18:57:19 -0600

    Add animated loading overlay for lazy-loaded routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,72 @@
+import { Suspense, lazy } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import Index from "./pages/Index";
-import Services from "./pages/Services";
-import Blog from "./pages/Blog";
-import Vamp from "./pages/Vamp";
-import ThreeDS from "./pages/ThreeDS";
-import ThreeDS2 from "./pages/ThreeDS2";
-import AboutPage from "./pages/AboutPage";
-import Terms from "./pages/Terms";
-import Privacy from "./pages/Privacy";
-import Apply from "./pages/Apply";
-import Security from "./pages/Security";
-import ComingSoon from "./pages/ComingSoon";
-import NotFound from "./pages/NotFound";
-import Quote from "./pages/Quote";
-import DeveloperPortal from "./pages/DeveloperPortal";
-import AccountInformation from "./pages/AccountInformation";
-import AddressVerification from "./pages/AddressVerification";
-import AutomaticCardUpdater from "./pages/AutomaticCardUpdater";
-import AutomaticLevelIII from "./pages/AutomaticLevelIII";
-import BillingStatement from "./pages/BillingStatement";
-import BlindCredits from "./pages/BlindCredits";
-import CardIDVerification from "./pages/CardIDVerification";
-import CountryCurrencyConfiguration from "./pages/CountryCurrencyConfiguration";
-import CustomerFeeConfiguration from "./pages/CustomerFeeConfiguration";
-import DkimSpfValidation from "./pages/DkimSpfValidation";
-import EmailTemplateChanges from "./pages/EmailTemplateChanges";
-import IntegrationOverview from "./pages/IntegrationOverview";
-import InvoiceLookFeel from "./pages/InvoiceLookFeel";
-import ManagingPartnerUsers from "./pages/ManagingPartnerUsers";
-import MerchantDefinedFields from "./pages/MerchantDefinedFields";
-import MerchantIPRestrictions from "./pages/MerchantIPRestrictions";
-import MerchantServiceProviderContacts from "./pages/MerchantServiceProviderContacts";
-import MerchantUserAccounts from "./pages/MerchantUserAccounts";
-import PasswordPolicy90Day from "./pages/PasswordPolicy90Day";
-import DirectConnect from "./pages/DirectConnect";
-import GatewayJs from "./pages/GatewayJs";
-import ReportConfiguration from "./pages/ReportConfiguration";
-import SecCodeConfiguration from "./pages/SecCodeConfiguration";
-import SettlementSchedule from "./pages/SettlementSchedule";
-import SpfDkimRecords from "./pages/SpfDkimRecords";
-import Secure3DS from "./pages/Secure3DS";
-import SubscriptionReports from "./pages/SubscriptionReports";
-import TestMode from "./pages/TestMode";
-import TheMerchantPortal from "./pages/TheMerchantPortal";
-import TransactionReports from "./pages/TransactionReports";
-import TransactionRouting from "./pages/TransactionRouting";
-import TwoFactorAuthentication from "./pages/TwoFactorAuthentication";
-import WhatIsACH from "./pages/WhatIsACH";
-import KountFraudManagement from "./pages/KountFraudManagement";
-import CollectCheckout from "./pages/CollectCheckout";
-import CollectJs from "./pages/CollectJs";
-import CustomerPresentCloud from "./pages/CustomerPresentCloud";
-import HostedPaymentPage from "./pages/HostedPaymentPage";
-import MobileSDK from "./pages/MobileSDK";
-import PaymentAPI from "./pages/PaymentAPI";
-import PaymentDeviceSDK from "./pages/PaymentDeviceSDK";
-import QueryAPI from "./pages/QueryAPI";
-import ThreeStepRedirect from "./pages/ThreeStepRedirect";
-import Webhooks from "./pages/Webhooks";
+import LoadingScreen from "./components/LoadingScreen";
+import RouteChangeLoader from "./components/RouteChangeLoader";
+
+const Index = lazy(() => import("./pages/Index"));
+const Services = lazy(() => import("./pages/Services"));
+const Blog = lazy(() => import("./pages/Blog"));
+const Vamp = lazy(() => import("./pages/Vamp"));
+const ThreeDS = lazy(() => import("./pages/ThreeDS"));
+const ThreeDS2 = lazy(() => import("./pages/ThreeDS2"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const Terms = lazy(() => import("./pages/Terms"));
+const Privacy = lazy(() => import("./pages/Privacy"));
+const Apply = lazy(() => import("./pages/Apply"));
+const Security = lazy(() => import("./pages/Security"));
+const ComingSoon = lazy(() => import("./pages/ComingSoon"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Quote = lazy(() => import("./pages/Quote"));
+const DeveloperPortal = lazy(() => import("./pages/DeveloperPortal"));
+const AccountInformation = lazy(() => import("./pages/AccountInformation"));
+const AddressVerification = lazy(() => import("./pages/AddressVerification"));
+const AutomaticCardUpdater = lazy(() => import("./pages/AutomaticCardUpdater"));
+const AutomaticLevelIII = lazy(() => import("./pages/AutomaticLevelIII"));
+const BillingStatement = lazy(() => import("./pages/BillingStatement"));
+const BlindCredits = lazy(() => import("./pages/BlindCredits"));
+const CardIDVerification = lazy(() => import("./pages/CardIDVerification"));
+const CountryCurrencyConfiguration = lazy(() => import("./pages/CountryCurrencyConfiguration"));
+const CustomerFeeConfiguration = lazy(() => import("./pages/CustomerFeeConfiguration"));
+const DkimSpfValidation = lazy(() => import("./pages/DkimSpfValidation"));
+const EmailTemplateChanges = lazy(() => import("./pages/EmailTemplateChanges"));
+const IntegrationOverview = lazy(() => import("./pages/IntegrationOverview"));
+const InvoiceLookFeel = lazy(() => import("./pages/InvoiceLookFeel"));
+const ManagingPartnerUsers = lazy(() => import("./pages/ManagingPartnerUsers"));
+const MerchantDefinedFields = lazy(() => import("./pages/MerchantDefinedFields"));
+const MerchantIPRestrictions = lazy(() => import("./pages/MerchantIPRestrictions"));
+const MerchantServiceProviderContacts = lazy(() => import("./pages/MerchantServiceProviderContacts"));
+const MerchantUserAccounts = lazy(() => import("./pages/MerchantUserAccounts"));
+const PasswordPolicy90Day = lazy(() => import("./pages/PasswordPolicy90Day"));
+const DirectConnect = lazy(() => import("./pages/DirectConnect"));
+const GatewayJs = lazy(() => import("./pages/GatewayJs"));
+const ReportConfiguration = lazy(() => import("./pages/ReportConfiguration"));
+const SecCodeConfiguration = lazy(() => import("./pages/SecCodeConfiguration"));
+const SettlementSchedule = lazy(() => import("./pages/SettlementSchedule"));
+const SpfDkimRecords = lazy(() => import("./pages/SpfDkimRecords"));
+const Secure3DS = lazy(() => import("./pages/Secure3DS"));
+const SubscriptionReports = lazy(() => import("./pages/SubscriptionReports"));
+const TestMode = lazy(() => import("./pages/TestMode"));
+const TheMerchantPortal = lazy(() => import("./pages/TheMerchantPortal"));
+const TransactionReports = lazy(() => import("./pages/TransactionReports"));
+const TransactionRouting = lazy(() => import("./pages/TransactionRouting"));
+const TwoFactorAuthentication = lazy(() => import("./pages/TwoFactorAuthentication"));
+const WhatIsACH = lazy(() => import("./pages/WhatIsACH"));
+const KountFraudManagement = lazy(() => import("./pages/KountFraudManagement"));
+const CollectCheckout = lazy(() => import("./pages/CollectCheckout"));
+const CollectJs = lazy(() => import("./pages/CollectJs"));
+const CustomerPresentCloud = lazy(() => import("./pages/CustomerPresentCloud"));
+const HostedPaymentPage = lazy(() => import("./pages/HostedPaymentPage"));
+const MobileSDK = lazy(() => import("./pages/MobileSDK"));
+const PaymentAPI = lazy(() => import("./pages/PaymentAPI"));
+const PaymentDeviceSDK = lazy(() => import("./pages/PaymentDeviceSDK"));
+const QueryAPI = lazy(() => import("./pages/QueryAPI"));
+const ThreeStepRedirect = lazy(() => import("./pages/ThreeStepRedirect"));
+const Webhooks = lazy(() => import("./pages/Webhooks"));
 
 const queryClient = new QueryClient();
 
@@ -73,67 +77,70 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/vamp" element={<Vamp />} />
-            <Route path="/3ds" element={<ThreeDS />} />
-            <Route path="/3ds2" element={<ThreeDS2 />} />
-            <Route path="/apply" element={<Apply />} />
-            <Route path="/quote" element={<Quote />} />
-            <Route path="/security" element={<Security />} />
-            <Route path="/developer-guides" element={<DeveloperPortal />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/TheMerchantPortal" element={<TheMerchantPortal />} />
-            <Route path="/IntegrationOverview" element={<IntegrationOverview />} />
-            <Route path="/DirectConnect" element={<DirectConnect />} />
-            <Route path="/GatewayJs" element={<GatewayJs />} />
-            <Route path="/Secure3DS" element={<Secure3DS />} />
-            <Route path="/KountFraudManagement" element={<KountFraudManagement />} />
-            <Route path="/AccountInformation" element={<AccountInformation />} />
-            <Route path="/AddressVerification" element={<AddressVerification />} />
-            <Route path="/AutomaticCardUpdater" element={<AutomaticCardUpdater />} />
-            <Route path="/AutomaticLevelIII" element={<AutomaticLevelIII />} />
-            <Route path="/BillingStatement" element={<BillingStatement />} />
-            <Route path="/BlindCredits" element={<BlindCredits />} />
-            <Route path="/CardIDVerification" element={<CardIDVerification />} />
-            <Route path="/CountryCurrencyConfiguration" element={<CountryCurrencyConfiguration />} />
-            <Route path="/CustomerFeeConfiguration" element={<CustomerFeeConfiguration />} />
-            <Route path="/DkimSpfValidation" element={<DkimSpfValidation />} />
-            <Route path="/EmailTemplateChanges" element={<EmailTemplateChanges />} />
-            <Route path="/InvoiceLookFeel" element={<InvoiceLookFeel />} />
-            <Route path="/ManagingPartnerUsers" element={<ManagingPartnerUsers />} />
-            <Route path="/MerchantDefinedFields" element={<MerchantDefinedFields />} />
-            <Route path="/MerchantIPRestrictions" element={<MerchantIPRestrictions />} />
-            <Route path="/MerchantServiceProviderContacts" element={<MerchantServiceProviderContacts />} />
-            <Route path="/MerchantUserAccounts" element={<MerchantUserAccounts />} />
-            <Route path="/PasswordPolicy90Day" element={<PasswordPolicy90Day />} />
-            <Route path="/ReportConfiguration" element={<ReportConfiguration />} />
-            <Route path="/SecCodeConfiguration" element={<SecCodeConfiguration />} />
-            <Route path="/SettlementSchedule" element={<SettlementSchedule />} />
-            <Route path="/SpfDkimRecords" element={<SpfDkimRecords />} />
-            <Route path="/SubscriptionReports" element={<SubscriptionReports />} />
-            <Route path="/TestMode" element={<TestMode />} />
-            <Route path="/TransactionReports" element={<TransactionReports />} />
-            <Route path="/TransactionRouting" element={<TransactionRouting />} />
-            <Route path="/TwoFactorAuthentication" element={<TwoFactorAuthentication />} />
-            <Route path="/WhatIsACH" element={<WhatIsACH />} />
-            <Route path="/CollectCheckout" element={<CollectCheckout />} />
-            <Route path="/CollectJs" element={<CollectJs />} />
-            <Route path="/CustomerPresentCloud" element={<CustomerPresentCloud />} />
-            <Route path="/HostedPaymentPage" element={<HostedPaymentPage />} />
-            <Route path="/MobileSDK" element={<MobileSDK />} />
-            <Route path="/PaymentAPI" element={<PaymentAPI />} />
-            <Route path="/PaymentDeviceSDK" element={<PaymentDeviceSDK />} />
-            <Route path="/QueryAPI" element={<QueryAPI />} />
-            <Route path="/ThreeStepRedirect" element={<ThreeStepRedirect />} />
-            <Route path="/Webhooks" element={<Webhooks />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <RouteChangeLoader />
+          <Suspense fallback={<LoadingScreen />}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/vamp" element={<Vamp />} />
+              <Route path="/3ds" element={<ThreeDS />} />
+              <Route path="/3ds2" element={<ThreeDS2 />} />
+              <Route path="/apply" element={<Apply />} />
+              <Route path="/quote" element={<Quote />} />
+              <Route path="/security" element={<Security />} />
+              <Route path="/developer-guides" element={<DeveloperPortal />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/TheMerchantPortal" element={<TheMerchantPortal />} />
+              <Route path="/IntegrationOverview" element={<IntegrationOverview />} />
+              <Route path="/DirectConnect" element={<DirectConnect />} />
+              <Route path="/GatewayJs" element={<GatewayJs />} />
+              <Route path="/Secure3DS" element={<Secure3DS />} />
+              <Route path="/KountFraudManagement" element={<KountFraudManagement />} />
+              <Route path="/AccountInformation" element={<AccountInformation />} />
+              <Route path="/AddressVerification" element={<AddressVerification />} />
+              <Route path="/AutomaticCardUpdater" element={<AutomaticCardUpdater />} />
+              <Route path="/AutomaticLevelIII" element={<AutomaticLevelIII />} />
+              <Route path="/BillingStatement" element={<BillingStatement />} />
+              <Route path="/BlindCredits" element={<BlindCredits />} />
+              <Route path="/CardIDVerification" element={<CardIDVerification />} />
+              <Route path="/CountryCurrencyConfiguration" element={<CountryCurrencyConfiguration />} />
+              <Route path="/CustomerFeeConfiguration" element={<CustomerFeeConfiguration />} />
+              <Route path="/DkimSpfValidation" element={<DkimSpfValidation />} />
+              <Route path="/EmailTemplateChanges" element={<EmailTemplateChanges />} />
+              <Route path="/InvoiceLookFeel" element={<InvoiceLookFeel />} />
+              <Route path="/ManagingPartnerUsers" element={<ManagingPartnerUsers />} />
+              <Route path="/MerchantDefinedFields" element={<MerchantDefinedFields />} />
+              <Route path="/MerchantIPRestrictions" element={<MerchantIPRestrictions />} />
+              <Route path="/MerchantServiceProviderContacts" element={<MerchantServiceProviderContacts />} />
+              <Route path="/MerchantUserAccounts" element={<MerchantUserAccounts />} />
+              <Route path="/PasswordPolicy90Day" element={<PasswordPolicy90Day />} />
+              <Route path="/ReportConfiguration" element={<ReportConfiguration />} />
+              <Route path="/SecCodeConfiguration" element={<SecCodeConfiguration />} />
+              <Route path="/SettlementSchedule" element={<SettlementSchedule />} />
+              <Route path="/SpfDkimRecords" element={<SpfDkimRecords />} />
+              <Route path="/SubscriptionReports" element={<SubscriptionReports />} />
+              <Route path="/TestMode" element={<TestMode />} />
+              <Route path="/TransactionReports" element={<TransactionReports />} />
+              <Route path="/TransactionRouting" element={<TransactionRouting />} />
+              <Route path="/TwoFactorAuthentication" element={<TwoFactorAuthentication />} />
+              <Route path="/WhatIsACH" element={<WhatIsACH />} />
+              <Route path="/CollectCheckout" element={<CollectCheckout />} />
+              <Route path="/CollectJs" element={<CollectJs />} />
+              <Route path="/CustomerPresentCloud" element={<CustomerPresentCloud />} />
+              <Route path="/HostedPaymentPage" element={<HostedPaymentPage />} />
+              <Route path="/MobileSDK" element={<MobileSDK />} />
+              <Route path="/PaymentAPI" element={<PaymentAPI />} />
+              <Route path="/PaymentDeviceSDK" element={<PaymentDeviceSDK />} />
+              <Route path="/QueryAPI" element={<QueryAPI />} />
+              <Route path="/ThreeStepRedirect" element={<ThreeStepRedirect />} />
+              <Route path="/Webhooks" element={<Webhooks />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,25 @@
+import type { CSSProperties } from "react";
+
+interface LoadingScreenProps {
+  variant?: "full" | "page";
+}
+
+const overlayStyle: Record<NonNullable<LoadingScreenProps["variant"]>, CSSProperties> = {
+  full: { background: "hsla(var(--background) / 0.92)" },
+  page: { background: "hsla(var(--background) / 0.78)" },
+};
+
+const LoadingScreen = ({ variant = "full" }: LoadingScreenProps) => (
+  <div
+    className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xl"
+    style={overlayStyle[variant]}
+  >
+    <div className="loading-shell">
+      <div className="loading-ring" aria-hidden />
+      <div className="loading-core" aria-hidden />
+      <p className="loading-label">Loading experience</p>
+    </div>
+  </div>
+);
+
+export default LoadingScreen;
